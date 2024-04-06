5c6e731d55c413be777fd116e66bb7cfa760f913
emil-nabil333 <elbrinselomda@yahoo.com>
2024-04-06 20:50:06 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,12 +1,20 @@
 #!/bin/sh # 
  # # Command: wget https://raw.githubusercontent.com/emilnabil/picon-other/main/installer.sh -qO - | /bin/sh # # ########################################### ###########################################  
 MY_URL="https://raw.githubusercontent.com/emilnabil/picon-other/main"  
-PICONCAM="piconCam.tar.gz"
-PICONSAT="piconSat.tar.gz"
-PICONCRYPT="piconCrypt.tar.gz"
-PICONPROV="piconProv.tar.gz"
-EMU="emu.tar.gz"
+PICONCAM="piconCam.zip"
+PICONSAT="piconSat.zip"
+PICONCRYPT="piconCrypt.zip"
+PICONPROV="piconProv.zip"
+EMU="emu.zip"
 echo "******************************************************************************************************************"
+#check mounted storage
+mrp=("/media/hdd" "/media/usb" "/media/mmc")
+for mr in "${mrp[@]}"; do
+if [ -d "$mr" ]; then
+echo ""
+break
+fi
+done
 echo "    Download And Install Picons  "
 echo "============================================================================================================================="
 #####################################################################################
@@ -15,44 +23,52 @@ cd /tmp
 set -e 
 wget "$MY_URL/$PICONCAM"
 wait
-tar -xzf $PICONCAM  -C /
+unzip piconCam.zip
 wait
+cp -rf /tmp/piconCam $mr/piconCam
 cd ..
 set +e
 rm -f /tmp/$PICONCAM
+rm -rf /tmp/piconCam
 echo "==========================================================================================================================="
 echo "         INSTALL PICON-SAT    "
 cd /tmp
 set -e 
 wget "$MY_URL/$PICONSAT"
 wait
-tar -xzf $PICONSAT  -C /
+unzip piconSat.zip
 wait
+cp -rf /tmp/piconSat $mr/piconSat
 cd ..
 set +e
 rm -f /tmp/$PICONSAT
+rm -rf /tmp/piconSat
 echo "****************************************************************************************************************************"
 echo "         INSTALL PICON-Prov    "
 cd /tmp
 set -e 
 wget "$MY_URL/$PICONPROV"
 wait
-tar -xzf $PICONPROV  -C /
+unzip piconProv.zip
 wait
+cp -rf /tmp/piconProv $mr/piconProv
 cd ..
 set +e
 rm -f /tmp/$PICONPROV
+rm -rf /tmp/piconProv
 echo "****************************************************************************************************************************"
 echo "         Install Picon-Crypt    "
 cd /tmp
 set -e 
 wget "$MY_URL/$PICONCRYPT"
 wait
-tar -xzf $PICONCRYPT  -C /
+unzip piconCrypt.zip
 wait
+cp -rf /tmp/piconCrypt $mr/piconCrypt
 cd ..
 set +e
 rm -f /tmp/$PICONCRYPT
+rm -rf /tmp/piconCrypt
 echo "==========================================================================================================================="
 echo "****************************************************************************************************************************"
 echo "         Install Picon-Emu    "
@@ -60,11 +76,13 @@ cd /tmp
 set -e 
 wget "$MY_URL/$EMU"
 wait
-tar -xzf $EMU  -C /
+unzip emu.zip
 wait
+cp -rf /tmp/emu $mr/emu
 cd ..
 set +e
 rm -f /tmp/$EMU
+rm -rf /tmp/emu
 echo "==========================================================================================================================="
 echo "# PICON  INSTALLED SUCCESSFULLY #"
 echo "
@@ -79,6 +97,14 @@ echo "**************************************************************************
 wait
 killall -9 enigma2
 exit 0
+
+
+
+
+
+
+
+
 
 
 
